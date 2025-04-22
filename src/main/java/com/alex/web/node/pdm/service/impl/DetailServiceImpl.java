b45aa5c9a3b1d3994eddf4af35eaf818bc2e3749
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-04-22 22:06:20 +0300

    1.Added logs using SimpleLoggingFacadeForJava->Logback.

--- src/main/java/com/alex/web/node/pdm/service/impl/DetailServiceImpl.java
+++ src/main/java/com/alex/web/node/pdm/service/impl/DetailServiceImpl.java
@@ -13,6 +13,7 @@ import com.alex.web.node.pdm.repository.detail.DetailRepository;
 import com.alex.web.node.pdm.search.DetailSearchDto;
 import com.alex.web.node.pdm.service.DetailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -32,27 +34,37 @@ public class DetailServiceImpl implements DetailService {
 
     @Override
     public DetailDto findById(Long id) {
-        return detailRepository.findDetailById(id)
+        log.debug("The method 'findById' with arg:{}", id);
+        DetailDto dto=detailRepository.findDetailById(id)
                 .orElseThrow(() -> new EntityNotFoundException("The detail with id '%1$d' is not found".formatted(id)));
+        log.info("Founded detail by id:{}", dto);
+        return dto;
     }
 
     @Override
     public Page<DetailDto> findAllByPageable(Pageable pageable) {
-        return detailRepository.findDetailsBy(pageable);
+        log.debug("The method 'findAllByPageable' with arg:{}", pageable);
+        Page<DetailDto> dtoPage = detailRepository.findDetailsBy(pageable);
+        log.info("Founded page of details by Pageable:{}", dtoPage);
+        return dtoPage;
     }
 
     @Override
     public List<DetailDto> findAllByIds(DetailSearchDto searchDto) {
-        return detailRepository.findDetailsByIds(searchDto.ids());
+        log.debug("The method 'findAllByIds' with arg:{}", searchDto);
+        List<DetailDto> dtoList = detailRepository.findDetailsByIds(searchDto.ids());
+        log.info("Founded list of detail by ids:{}", dtoList);
+        return dtoList;
     }
 
     @Override
     @Transactional
     public DetailDto create(NewDetailDto newDetailDto) {
+        log.debug("The method 'create' with arg:{}", newDetailDto);
         if (detailRepository.existsDetailByName(newDetailDto.name())) {
             throw new NameAlreadyExistsException("The detail with name '%1$s' already exists".formatted(newDetailDto.name()));
         }
-        return specificationRepository.findById(newDetailDto.specificationId())
+        DetailDto savedDto = specificationRepository.findById(newDetailDto.specificationId())
                 .map(spec -> {
                     DetailDto savedDetailDto = detailRepository.saveDetail(newDetailDto);
                     Integer newAmountSpec = recalculateAmount(spec);
@@ -61,6 +73,8 @@ public class DetailServiceImpl implements DetailService {
                     return savedDetailDto;
                 }).orElseThrow(() -> new EntityCreationException("The detail with name '%1$s' creation error"
                         .formatted(newDetailDto.name())));
+        log.info("A new detail with id:{} has been created",savedDto.id());
+        return savedDto;
 
 
     }
@@ -78,13 +92,14 @@ public class DetailServiceImpl implements DetailService {
     @Override
     @Transactional
     public DetailDto update(Long id, UpdateDetailDto dto) {
+        log.debug("The method 'update' with arg:{}", dto);
         DetailDto detailDto = detailRepository.findDetailById(id)
                 .orElseThrow(() -> new EntityNotFoundException("The detail with id '%1$d' is not found".formatted(id)));
 
         if (!(dto.name().equals(detailDto.name())) && detailRepository.existsDetailByName(dto.name()))
             throw new NameAlreadyExistsException("The detail with name '%1$s' already exists".formatted(dto.name()));
 
-        return Optional.of(detailDto)
+        DetailDto updatedDto=Optional.of(detailDto)
                 .flatMap(detail -> specificationRepository.findById(detailDto.specificationId()))
                 .map(spec -> {
                     DetailDto updatedDetailDto = detailRepository.updateDetail(id, dto);
@@ -93,7 +108,9 @@ public class DetailServiceImpl implements DetailService {
                     specificationRepository.flush();
                     return updatedDetailDto;
                 })
-                .orElseThrow(() -> new EntityNotFoundException("The spec with id '%1$d' is not found".formatted(detailDto.id())));
+                .orElseThrow(() -> new EntityNotFoundException("The detail with id '%1$d' is not found".formatted(id)));
+        log.info("The specification with id:{} has been updated",id);
+        return updatedDto;
     }
 
     private DetailDto tryFindDetailById(Long id) {
@@ -109,6 +126,7 @@ public class DetailServiceImpl implements DetailService {
     @Override
     @Transactional
     public boolean delete(Long id) {
+        log.debug("The method 'delete' with args:{}",id);
         return detailRepository.findDetailById(id)
                 .map(detail -> detailRepository.deleteDetail(id))
                 .orElseThrow(() -> new EntityNotFoundException("The detail with id '%1$d' is not found".formatted(id)));
@@ -116,6 +134,7 @@ public class DetailServiceImpl implements DetailService {
 
     @Override
     public List<DetailDto> findAllBySpecId(Long specId) {
+        log.debug("The method 'findAllBySpecId' with args:{}",specId);
         return detailRepository.findDetailsBySpecificationId(specId);
     }
 
