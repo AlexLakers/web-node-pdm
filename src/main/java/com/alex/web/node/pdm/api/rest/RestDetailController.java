ccc889c5d5bc1c5622a9a5ac7a46127665e2242e
AlexLakers <alexey.nefedov199321@yandex.ru>
2025-04-25 23:11:00 +0300

    1.Added docs.

--- src/main/java/com/alex/web/node/pdm/api/rest/RestDetailController.java
+++ src/main/java/com/alex/web/node/pdm/api/rest/RestDetailController.java
@@ -4,6 +4,7 @@ package com.alex.web.node.pdm.api.rest;
 import com.alex.web.node.pdm.dto.detail.DetailDto;
 import com.alex.web.node.pdm.dto.detail.NewDetailDto;
 import com.alex.web.node.pdm.dto.detail.UpdateDetailDto;
+import com.alex.web.node.pdm.model.Detail;
 import com.alex.web.node.pdm.service.DetailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * This is a REST-controller.It provides set of endpoints for using the main operation with {@link Detail detail}.
+ */
+
 @Slf4j
 @RestController
 @RequestMapping("api/v1/details")
@@ -26,12 +31,25 @@ import org.springframework.web.bind.annotation.*;
 public class RestDetailController {
     private final DetailService detailService;
 
+    /**
+     * Returns status and output-dto formatted .JSON by id of detail.
+     *
+     * @param id of detail.
+     * @return output-dto.
+     */
+
     @GetMapping("/{id}")
     public ResponseEntity<DetailDto> findById(@PathVariable Long id) {
         log.info("--start 'find detail by id' rest endpoint--");
         return ResponseEntity.status(HttpStatus.OK).body(detailService.findById(id));
     }
 
+    /**
+     * Returns status and page of output-dto formatted .JSON by pageable
+     *
+     * @param pageable input-pageable.
+     * @return page of output-dto.
+     */
 
     @GetMapping
     public ResponseEntity<Page<DetailDto>> findAllByPageable(@PageableDefault(page = 0, size = 20)
@@ -41,6 +59,12 @@ public class RestDetailController {
         return ResponseEntity.ok().body(detailService.findAllByPageable(pageable));
     }
 
+    /**
+     * Returns status and output-dto formatted .JSON of created entity.
+     *
+     * @param newDetailDto input-dto.
+     * @return output-dto.
+     */
 
     @PostMapping
     public ResponseEntity<DetailDto> create(@Validated @RequestBody NewDetailDto newDetailDto) {
@@ -52,7 +76,13 @@ public class RestDetailController {
     }
 
 
-
+    /**
+     * Returns status and updated output-dto formatted .JSON by input-dto and id of detail.
+     *
+     * @param id              id of detail.
+     * @param updateDetailDto input-dto.
+     * @return output-dto.
+     */
     @PutMapping("/{id}")
     public ResponseEntity<DetailDto> update(@PathVariable Long id,
                                             @Validated @RequestBody UpdateDetailDto updateDetailDto) {
@@ -61,6 +91,12 @@ public class RestDetailController {
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(detailService.update(id, updateDetailDto));
     }
+    /**
+     * Returns status of deleting operation.
+     *
+     * @param id of detail.
+     * @return output-dto.
+     */
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
--- src/main/java/com/alex/web/node/pdm/api/rest/RestSpecificationController.java
+++ src/main/java/com/alex/web/node/pdm/api/rest/RestSpecificationController.java
@@ -4,6 +4,7 @@ import com.alex.web.node.pdm.dto.detail.DetailDto;
 import com.alex.web.node.pdm.dto.specification.NewSpecificationDto;
 import com.alex.web.node.pdm.dto.specification.SpecificationDto;
 import com.alex.web.node.pdm.dto.specification.UpdateSpecificationDto;
+import com.alex.web.node.pdm.model.Specification;
 import com.alex.web.node.pdm.search.SpecificationSearchDto;
 import com.alex.web.node.pdm.service.DetailService;
 import com.alex.web.node.pdm.service.SpecificationService;
@@ -26,6 +27,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * This is a REST-controller.It provides set of endpoints for using the main operation with {@link Specification specification.
+ */
+
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/specifications")
@@ -33,6 +38,13 @@ import java.util.List;
 public class RestSpecificationController {
     private final SpecificationService specificationService;
     private final DetailService detailService;
+
+    /**
+     * Returns status and page of output-dto formatted .JSON by input-search-dto.
+     *
+     * @param specificationSearchDto input-search-dto.
+     * @return page of output-dto.
+     */
 
     @GetMapping
     public ResponseEntity<Page<SpecificationDto>> findAll(/*@RequestParam(defaultValue = "0") Integer pageNumber,
@@ -42,17 +54,33 @@ public class RestSpecificationController {
                                                           @SortDefault(sort = "id",direction = Sort.Direction.ASC) Pageable pageable*/) {
         log.info("--start 'find all specifications' rest endpoint--");
         return ResponseEntity.status(HttpStatus.OK)
-                .header(HttpHeaders.CONTENT_TYPE,MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .body(specificationService.findAll(specificationSearchDto));
     }
+
+    /**
+     * Returns status and list of output-dto of detail formatted .JSON by id of specification
+     *
+     * @param specId id of specification.
+     * @return list of output-dto.
+     */
+
     @GetMapping("/{specId}/details")
-    public ResponseEntity<List<DetailDto>> findAllDetailsBySpecId(@PathVariable Long specId){
+    public ResponseEntity<List<DetailDto>> findAllDetailsBySpecId(@PathVariable Long specId) {
         log.info("--start 'find all detail by specification id' rest endpoint--");
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(detailService.findAllBySpecId(specId));
     }
+
+    /**
+     * Returns status and output-dto formatted .JSON by id of specification.
+     *
+     * @param id of specification.
+     * @return output-dto.
+     */
+
     @GetMapping("/{id}")
     public ResponseEntity<SpecificationDto> findById(@PathVariable("id") Long id) {
         log.info("--start 'find specification by id' rest endpoint--");
@@ -69,25 +97,50 @@ public class RestSpecificationController {
      public ResponseEntity<List<SpecificationDto>> findByUserId(@RequestParam("userId") Long userId) {
          return ResponseEntity.status(HttpStatus.OK).body(specificationService.findAllByUserId(userId));
      }*/
+
+    /**
+     * Returns status and output-dto formatted .JSON of created entity.
+     *
+     * @param newSpecificationDto input-dto.
+     * @return output-dto.
+     */
+
     @PostMapping
-    public ResponseEntity<SpecificationDto> create(@RequestBody @Validated NewSpecificationDto newSpecificationDto){
+    public ResponseEntity<SpecificationDto> create(@RequestBody @Validated NewSpecificationDto newSpecificationDto) {
         log.info("--start 'create a new specification' rest endpoint--");
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .header(HttpHeaders.CONTENT_TYPE,MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .body(specificationService.create(newSpecificationDto));
     }
+
+    /**
+     * Returns status and updated output-dto formatted .JSON by input-dto and id of specification.
+     *
+     * @param id                     of specification.
+     * @param updateSpecificationDto input-dto.
+     * @return output-dto.
+     */
+
     @PutMapping("/{id}")
     public ResponseEntity<SpecificationDto> update(@PathVariable Long id,
-                                                   @RequestBody @Validated UpdateSpecificationDto updateSpecificationDto){
+                                                   @RequestBody @Validated UpdateSpecificationDto updateSpecificationDto) {
         log.info("--start 'update specification by id' rest endpoint--");
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .header(HttpHeaders.CONTENT_TYPE,MediaType.APPLICATION_JSON_VALUE)
-                .body(specificationService.update(id,updateSpecificationDto));
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(specificationService.update(id, updateSpecificationDto));
     }
+
+    /**
+     * Returns status of deleting operation.
+     *
+     * @param id id of specification.
+     * @return output-dto.
+     */
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<SpecificationDto> delete(@PathVariable Long id){
+    public ResponseEntity<SpecificationDto> delete(@PathVariable Long id) {
         log.info("--start 'delete specification by id' rest endpoint--");
         specificationService.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
